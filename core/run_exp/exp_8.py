7f310373b753ea29fac0752d269941f8aad6e501
anle <vananle1993@gmail.com>
2023-08-08 21:26:30 +0900

    refactoring

--- core/run_exp/exp_8.py
+++ core/run_exp/exp_8.py
@@ -1,5 +1,5 @@
 import sys
-
+import matplotlib.pyplot as plt
 sys.path.append('../../')
 
 from core.run_exp.base_runner import *
@@ -7,8 +7,12 @@ from core.run_exp.base_runner import *
 warnings.simplefilter("ignore")
 warnings.filterwarnings("ignore", category=UserWarning)
 
+logs_path = '../../results/core/'
+results_plot_path = '../../results_plot/core'
+if not os.path.exists(results_plot_path):
+    os.makedirs(results_plot_path)
 
-def exp_8():
+def exp_8(datasets, models, mon_per, input_len, predict_len, colors, label_models, seeds):
     print('RUNNNING EXP8')
 
     args = utils.get_args()
@@ -19,8 +23,6 @@ def exp_8():
 
     t1 = time.time()
     input_len = 15
-    datasets = ['germany', 'gnnet-40']
-    # datasets = ['abilene', 'geant']
     models = ['gwn']
     mon_per = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
     pre_len = 6
@@ -77,4 +79,70 @@ def exp_8():
     print('Date&Time: ', date.today())
 
 
-exp_8()
+
+def plot_exp8(datasets, models, input_len, predict_len, colors, label_models, seeds):
+    mon_per = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+    pre_len = 6
+    method = 'mtsr_cs'
+    mon_methods = ['topk', 'random', 'topk_random']
+    MON_METHODS = ['TOPK', 'RANDOM', 'PROPOSAL']
+    seeds = [20, 5, 1, 46, 77]
+    mon_per = np.array(mon_per)
+
+    colors = ['k', 'g', 'r']
+
+    for dataset_id, dataset in enumerate(datasets):
+
+        fig, ax = plt.subplots()
+        for mon_id, mon_method in enumerate(mon_methods):
+            results_mtsr = []
+            mae_mtsr = []
+            for i, seed in enumerate(seeds):
+
+                path = f'../results/core/exp8/{method}_{method}_{mon_method}_{dataset}_mae_y_cs_{seed}.txt'
+
+                data = np.loadtxt(path, delimiter=',')
+                if i == 0:
+                    data = np.expand_dims(data, axis=-1)
+                    mae_mtsr = data
+                else:
+                    data = np.expand_dims(data, axis=-1)
+                    mae_mtsr = np.concatenate((mae_mtsr, data), axis=-1)
+
+            mae_mtsr_mean = np.mean(mae_mtsr, axis=-1)
+            mae_mtsr_std = np.std(mae_mtsr, axis=-1)
+            if dataset == 'abilene':
+                mae_mtsr_mean = mae_mtsr_mean / 100
+                mae_mtsr_std = mae_mtsr_std / 100
+            else:
+                mae_mtsr_mean = mae_mtsr_mean / 300
+                mae_mtsr_std = mae_mtsr_std / 300
+
+            ax.errorbar(mon_per * 100, mae_mtsr_mean, mae_mtsr_std, label=f'{MON_METHODS[mon_id]}',
+                        color=colors[mon_id], linestyle="solid")
+
+        ax.legend()
+        ax.set_xlabel('Percentage of monitored flows (%)', fontsize=15)
+        ax.set_ylabel('Mean Absolute Error', fontsize=15)
+        plt.tick_params(axis='both', which='both', labelsize=12)
+        plt.savefig(os.path.join(results_plot_path, f'exp6_mae_{dataset}.svg'), dpi=300)
+        plt.close()
+
+
+
+if __name__ == "__main__":
+
+    input_len = 15
+    models = ['gwn']
+    mon_per = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+    predict_len = 6
+    method = 'mtsr_cs'
+    mon_method = 'topk_random'  # random, topk, topk_random, topk_per_node
+
+    datasets = ['germany', 'gnnet-40']
+    seeds = [20, 5, 1, 46, 77]
+    colors = ['r', 'g', 'k', 'b', 'm']
+    label_models = ['GWN', 'LSTM', 'GRU', 'STGCN', 'MTGNN']
+
+    # exp_1(datasets, models, input_len, predict_len, seeds)
+    plot_exp8(datasets, models, input_len, predict_len, colors, label_models, seeds)
