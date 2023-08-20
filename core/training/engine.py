873e46cd7bea0ea8941275f9225bb09dcc6a43df
anle <vananle1993@gmail.com>
2023-08-21 00:18:48 +0900

    refactoring

--- core/training/engine.py
+++ core/training/engine.py
@@ -104,7 +104,11 @@ class TrainEngine:
                     psiT = obj['psiT']
 
                 y_cs = np.zeros(shape=(y_hat.shape[0], self.args.num_flow))
+                run_time = []
+
                 for t in range(y_hat.shape[0]):
+                    time_s = time.time()
+
                     mon_index_t = mon_index[t]
 
                     phi = get_phi(mon_index_t, self.args.num_flow)
@@ -115,6 +119,10 @@ class TrainEngine:
                     ycs = np.dot(ShatT, psiT)
                     ycs[:, mon_index_t] = y_hat_t
                     y_cs[t] = ycs.flatten()  # shape(n, N_F)
+                    run_time.append(time.time() - time_s)
+
+                run_time = np.array(run_time)
+                np.save(f'/home/anle/mtsr-cs-runtime-{self.args.dataset}-{self.args.mon_per}.npy', run_time)
 
             elif self.args.method == 'mtsr_nocs':
                 y_cs = np.zeros(shape=(y_hat.shape[0], self.args.num_flow))
